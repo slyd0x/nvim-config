f4a60b3ef083ccaa4d0ff8efde0005c93d7605d1
slyd0x <github@slydox.de>
2024-04-23 11:34:36 +0200

    linting and formatting added

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,11 +1,10 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
-  
   config = function()
     local config = require("nvim-treesitter.configs")
     config.setup({
-      ensure_installed = {"c", "cpp", "lua", "javascript", "html", "python" },
+      auto_install = true,
       highlight = { enable = true },
       indent = { enable = true },
     })
