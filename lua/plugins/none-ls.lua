462903aa3e5469eb3e805bfbd9afd8f916f4bb3d
slyd0x <github@slydox.de>
2024-04-23 11:38:10 +0200

    fixed a clang formatting error

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -5,8 +5,6 @@ return {
     null_ls.setup({
       sources = {
         null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.clang_format,
-        null_ls.builtins.diagnostics.cpplint,
       },
     })
     vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
